1822635725f23b7b1104a0bca30c6e8c28ebe555
Brendan Abolins <b.abolins@gmail.com>
2023-05-19 18:25:55 -0400

    Added computation of the correlation function.

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ fn main() {
     let mut count: usize = 0;
     let mut mean = 0.0;
     let mut variance = 0.0;
+    let mut corr = vec![0.0; sites / 2];
     for i in 0..sample_iters {
         gibbs_sample(&mut z, j_beta, h_beta, sites);
 
@@ -29,31 +30,37 @@ fn main() {
             let delta = m - mean;
             mean += delta / count as f64;
             variance += delta * (m - mean);
+
+            let temp_corr = correlation(&z, sites);
+            for i in 0..sites / 2 {
+                let delta = temp_corr[i] - corr[i];
+                corr[i] += delta / count as f64;
+            }
         }
     }
     variance /= (count - 1) as f64;
 
     println!("observations = {count}, M = {mean}, std. dev. = {}", variance.sqrt());
+    println!("{:?}", corr);
 }
 
 fn gibbs_sample(z: &mut Vec<i32>, j_beta: f64, h_beta: f64, sites: usize) {
     let arg = z[sites - 1] + z[1];
-    let arg = -j_beta * arg as f64 + h_beta;
+    let arg = j_beta * arg as f64 + h_beta;
     let p = prob(arg);
     let u: f64 = rand::thread_rng().sample(Standard);
     z[0] = if u < p { 1 } else { -1 };
 
-
     for i in 1..sites - 1 {
         let arg = z[i - 1] + z[i + 1];
-        let arg = -j_beta * arg as f64 + h_beta;
+        let arg = j_beta * arg as f64 + h_beta;
         let p = prob(arg);
         let u: f64 = rand::thread_rng().sample(Standard);
         z[i] = if u < p { 1 } else { -1 };
     }
 
     let arg = z[sites - 2] + z[0];
-    let arg = -j_beta * arg as f64 + h_beta;
+    let arg = j_beta * arg as f64 + h_beta;
     let p = prob(arg);
     let u: f64 = rand::thread_rng().sample(Standard);
     z[sites - 1] = if u < p { 1 } else { -1 };
@@ -61,4 +68,15 @@ fn gibbs_sample(z: &mut Vec<i32>, j_beta: f64, h_beta: f64, sites: usize) {
 
 fn prob(arg: f64) -> f64 {
     arg.exp() / (2.0 * arg.cosh())
+}
+
+fn correlation(z: &Vec<i32>, sites: usize) -> Vec<f64> {
+    let mut corr: Vec<f64> = Vec::with_capacity(sites / 2);
+    let m2 = z.iter().map(|z| (z * z) as f64).sum::<f64>();
+    corr.push(m2 / sites as f64);
+    for i in 1..sites / 2 {
+        let m2 = (0..sites).map(|j| (z[j] * z[(j + i) % sites]) as f64).sum::<f64>();
+        corr.push(m2 / sites as f64);
+    }
+    corr
 }
